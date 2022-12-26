92913ab5212497d4cb1e299c44296bee4ae654e8
Minh Sang Tran <sangtran.d14ptit@outlook.com>
2022-12-26 16:24:46 +0700

    feat: Khởi tạo project

---
+++ Demo.UnitTest/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.App
+{
+    /// <summary>
+    /// Phép toán
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// Tính tổng 2 số
+        /// </summary>
+        /// <param name="x">Số hạng thứ nhất</param>
+        /// <param name="y">Số hạng thứ hai</param>
+        /// <returns>Tổng 2 số</returns>
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        /// <summary>
+        /// Tính hiệu 2 số
+        /// </summary>
+        /// <param name="x">Số bị trừ</param>
+        /// <param name="y">Số trừ</param>
+        /// <returns>Hiệu 2 số</returns>
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        /// <summary>
+        /// Tính tích 2 số
+        /// </summary>
+        /// <param name="x">Thừa số thứ nhất</param>
+        /// <param name="y">Thừa số thứ hai</param>
+        /// <returns>Tích 2 số</returns>
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        /// <summary>
+        /// Tính thương 2 số
+        /// </summary>
+        /// <param name="x">Số bị chia</param>
+        /// <param name="y">Số chia</param>
+        /// <returns>Thương 2 số</returns>
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ Demo.UnitTest/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ *  Chạy thử ứng dụng 
+ *  Tạo project NUnit: Sample.App.UnitTests
+ *  Tạo class CalculatorTests --> Test class Calculator
+ *  Thực hiện viết unit test cho 2 hàm Add và Divide
+ *  Thêm case test cho hàm Divide và refactor hàm Divide trong class Calculator
+ */
+
+using Sample.App;
+
+var calculator = new Calculator();
+int x, y;
+
+x = 1;
+y = 2;
+var resultAdd = calculator.Add(x, y);
+Console.WriteLine($"Ket qua phep cong: {x} + {y} = {resultAdd}");
+
+x = 4;
+y = 2;
+var resultDivide = calculator.Divide(4, 2);
+Console.WriteLine($"Ket qua phep chia: {x} / {y} = {resultDivide}");
